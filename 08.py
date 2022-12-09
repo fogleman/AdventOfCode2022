a2320e26122a4ca9fb5adb9ff68045e1d858e600
Michael Fogleman <fogleman@gmail.com>
2022-12-09 13:13:07 -0500

    day 8 cleanup

--- 08.py
+++ 08.py
@@ -1,43 +1,22 @@
 import fileinput
-import re
 
-grid = []
-for line in fileinput.input():
-    grid.append(list(map(int, line.rstrip())))
+grid = [line.rstrip() for line in fileinput.input()]
+w, h = len(grid[0]), len(grid)
 
-h = len(grid)
-w = len(grid[0])
-
-def visible(x, y):
-    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for dx, dy in dirs:
-        nx, ny = x, y
+def scan(x, y):
+    score, visible = 1, False
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nx, ny, d = x, y, 0
         while True:
             nx, ny = nx + dx, ny + dy
-            if nx < 0 or ny < 0 or nx > w - 1 or ny > h - 1:
-                return True
+            if nx < 0 or ny < 0 or nx >= w or ny >= h:
+                visible = True
+                break
+            d += 1
             if grid[ny][nx] >= grid[y][x]:
                 break
-    return False
+        score *= d
+    return visible, score
 
-def visible(x, y):
-    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    scores = []
-    for dx, dy in dirs:
-        nx, ny = x, y
-        result = 0
-        while True:
-            nx, ny = nx + dx, ny + dy
-            if nx < 0 or ny < 0 or nx > w - 1 or ny > h - 1:
-                break
-            result += 1
-            if grid[ny][nx] >= grid[y][x]:
-                break
-        scores.append(result)
-    return scores[0]*scores[1]*scores[2]*scores[3]
-
-counts = []
-for y in range(h):
-    for x in range(w):
-        counts.append(visible(x, y))
-print(max(counts))
+print(sum(scan(x, y)[0] for y in range(h) for x in range(w)))
+print(max(scan(x, y)[1] for y in range(h) for x in range(w)))
