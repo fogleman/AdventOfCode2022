4e1ab06b3ebec146156ecdf4e238a71cdee6338e
Michael Fogleman <fogleman@gmail.com>
2022-12-10 11:57:11 -0500

    day 10

--- 10.py
+++ 10.py
@@ -1,21 +1,18 @@
 import fileinput
 
 X = 1
-values = [X, X]
+values = [X]
 for line in fileinput.input():
     tokens = line.split()
-    op, args = tokens[0], tokens[1:]
-    if op == 'addx':
+    values.append(X)
+    if tokens[0] == 'addx':
         values.append(X)
-        X += int(args[0])
-        values.append(X)
-    else:
-        values.append(X)
+        X += int(tokens[1])
 
 print(sum((i * 40 + 20) * x for i, x in enumerate(values[20::40])))
 
 for y in range(6):
     for x in range(40):
         v = values[y * 40 + x + 1]
-        print('#' if abs(v - x) <= 1 else '.', end='')
+        print('#' if abs(v - x) < 2 else '.', end='')
     print()
